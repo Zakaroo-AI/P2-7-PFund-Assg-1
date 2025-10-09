8e13f1c2dc0d4562774c13e09ed390f28fd2ab52
Zakaroo-AI <leehongyih@gmail.com>
2025-10-09 16:16:12 +0800

    daily returns addition

--- data/fetch.py
+++ data/fetch.py
@@ -20,7 +20,8 @@ def get_stock_data(ticker=None, filepath=None):
         return df, label
 
     elif ticker:
-        data = yf.download(ticker, progress=False)
+        data = yf.Ticker(ticker).history(period='3y')
+        # data = yf.download(ticker, progress=False)
         if data.empty:
             raise ValueError(f"No data found for ticker '{ticker}'.")
         data.reset_index(inplace=True)
